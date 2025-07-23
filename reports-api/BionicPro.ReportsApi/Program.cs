271873a0cc45aa2ca1a73fd9f4df69ac93ce40c6
Ilarion Panzhin <Panzhin.I@eplan.de>
2025-07-23 19:32:12 +0200

    fix: cors

--- reports-api/BionicPro.ReportsApi/Program.cs
+++ reports-api/BionicPro.ReportsApi/Program.cs
@@ -1,35 +1,65 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3000")   
+            .AllowAnyHeader()                         
+            .AllowAnyMethod()                         
+            .AllowCredentials();
+    });
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        options.Authority = "http://keycloak:8080/realms/BionicPRO"; 
+        options.Authority = "http://keycloak:8080/realms/reports-realm"; 
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateAudience = false, 
             ValidateIssuer = true,
-            ValidIssuer = "http://keycloak:8080/realms/BionicPRO", 
+            ValidIssuers =
+            [
+                "http://localhost:8080/realms/reports-realm",
+                "http://keycloak:8080/realms/reports-realm"
+            ],
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-            RoleClaimType = "roles",
+            RoleClaimType = "realm_access.roles",
         };
     });
 
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("ProtheticOnly", policy =>
-        policy.RequireClaim("roles", "prothetic_user"));
+        policy.RequireAssertion(context =>
+        {
+            var rolesClaim = context.User.FindFirst("realm_access");
+            if (rolesClaim == null) return false;
+
+            var roles = System.Text.Json.JsonDocument.Parse(rolesClaim.Value)
+                .RootElement
+                .GetProperty("roles")
+                .EnumerateArray()
+                .Select(r => r.GetString());
+
+            return roles.Contains("prothetic_user");
+        }));
 });
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseCors("AllowReactApp");
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAuthentication();
 app.UseAuthorization();
 
